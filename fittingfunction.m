65d5bb96195d031e607b4f1f68d1ebe2138138b4
Gianni071 <giannisumma071@gmail.com>
2022-12-06 16:46:35 +0100

    Added Tip and Root airfoil .txt files and CST coefficients for tip airfoil

--- fittingfunction.m
+++ fittingfunction.m
@@ -1,5 +1,24 @@
 %Fitting function used in WhitcombFitting.m
 function[error] = fittingfunction(x)
 
-t = readmatrix('whitcomb135.dat');
+%Read .dat file
+txt = readmatrix('TipAirfoil.txt');
 
+%Read CST coefficients
+CSTup = x(1:length(x)/2);
+CSTlow = x(length(x)/2+1:length(x));
+
+k = length(txt)/2;
+%Reference airfoil coordinates
+Xcoords = txt(1:k,1);
+Yu = txt(1:k,2);
+Yl = txt(k+1:length(txt),2);
+
+%Class function coordinates
+[Xtu,Xtl] = D_airfoil2(CSTup,CSTlow,Xcoords);
+
+
+errorup = sum((Xtu(:,2)-Yu).^2);
+errorlow = sum((Xtl(:,2)-Yl).^2);
+
+error = errorup+errorlow;
