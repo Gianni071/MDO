88541ec585955ac8a1419b6104ec3d02cc6a2646
Gianni071 <giannisumma071@gmail.com>
2022-12-15 13:54:45 +0100

    fixed constraints

--- TankVolTest.m
+++ TankVolTest.m
@@ -5,12 +5,16 @@ close all
 CSTroot = readmatrix('RootRefCST.txt');
 CSTtip = readmatrix('TipRefCST.txt');
 
+y2 = 5.25; %Fix global
+Vaux = 1.174; %Fix global
 CST = [CSTroot;CSTtip];
 x = [CST;3.94;0.75;0.475;0;0;17.82;26.21;5000;10023;16];
 c1 = x(25);
 c2 = c1*x(26);
 c3 = c2*x(27);
-
+y3 = x(31)/2;
+Wfuel = x(33); %[kg]
+rhofuel = 0.81715*10^3; %[kg/m^3]
 
 RootUp = transpose(x(1:6));
 RootLow = transpose(x(7:12));
@@ -28,7 +32,7 @@ XtlRoot = XtlRt*c1;
 %Root airfoil area
 dx1 = XtuRoot(2,1)-XtuRoot(1,1);
 A1 = 0;
-for i = 11:30
+for i = 9:31
     tn = XtuRoot(i,2) - XtlRoot(i,2);
     tn1 = XtuRoot(i+1,2) - XtlRoot(i+1,2);
     Ai = dx1*(tn+tn1)/2;
@@ -40,7 +44,7 @@ XtuMid = XtuRt*c2;
 XtlMid = XtlRt*c2;
 dx2 = XtuMid(2,1)-XtuMid(1,1);
 A2 = 0;
-for i = 11:30
+for i = 9:31
     tn = XtuMid(i,2) - XtlMid(i,2);
     tn1 = XtuMid(i+1,2) - XtlMid(i+1,2);
     Ai = dx2*(tn+tn1)/2;
@@ -51,3 +55,43 @@ end
 %Outboard unit airfoil coordinates
 [Xtutip,Xtltip] = D_airfoil2(TipUp,TipLow,xairfoil);
 
+%Calculate c85 (85% semi-span) based on planform
+c85 = (c3-c2)/(y3-y2)*(0.9*y3 - y2) + c2;
+
+%85% span airfoil coordinates
+Xtutip = c85*Xtutip;
+Xtltip = c85*Xtltip;
+
+dx3 = Xtutip(2,1) - Xtutip(1,1);
+A3 = 0;
+
+%Loop to calculate cross-sectional area
+for i = 9:31
+    tn = Xtutip(i,2) - Xtltip(i,2);
+    tn1 = Xtutip(i+1,2) - Xtltip(i+1,2);
+    Ai = dx3*(tn+tn1)/2;
+    A3 = A3 + Ai;
+end
+
+%Tank Volume is average cross-sectional area times span
+
+%Inboard wing volume
+%Average area:
+Ain = (A1+A2)/2;
+
+%Volume
+Vin = Ain*y2;
+
+%Outboard wing volume
+%Average area:
+Aout = (A2+A3)/2;
+
+%Volume
+Vout = Aout*(0.9*y3 - y2);
+
+%Total volume of wing fuel tank:
+Vtot = 0.93*2*(Vin+Vout);
+
+%Calculate constraint
+Vreq = Wfuel/rhofuel;
+con2 = Wfuel/rhofuel - Vaux - Vtot;
--- constraints.m
+++ constraints.m
@@ -19,6 +19,7 @@ Vaux = data.Vaux; %[m^3]
 %x = [CST,c1,lambda1,lambda2,theta2,theta3,LEsw,b,Wwing,Wfuel,L/DcrAC]
 %x = [1-24,25, 26      27      28     29    30  31  32    33     34]
 
+
 CST = x(1:24);
 c1 = x(25);
 lambda1 = x(26);
@@ -27,6 +28,7 @@ b = x(31);
 Wwinghat = x(32);
 Wfuelhat = x(33);
 LDhat = x(34);
+
 
 y3 = b/2;
 c2 = lambda1*c1;
@@ -48,12 +50,94 @@ ceq3 = LDhat - LD;
 
 ceq = [ceq1;ceq2;ceq3];
 
-%% Inequality 1
+%% Inequality 1 Wing Loading
 WTO = Wfuel+Wwing+WAW;
 
 WScalc = WTO/S;
 
-c1 = WScalc - WSref;
+con1 = WScalc - WSref;
 
-%% Inequality 2
+%% Inequality 2 Fuel Tank Volume
+rhofuel = 0.81715*10^3; %[kg/m^3]
 
+RootUp = transpose(x(1:6));
+RootLow = transpose(x(7:12));
+TipUp = transpose(x(13:18));
+TipLow = transpose(x(19:24));
+
+xairfoil = transpose(linspace(0,1,51));
+
+%Root unit airfoil coordinates
+[XtuRt,XtlRt] = D_airfoil2(RootUp,RootLow,xairfoil);
+
+%Root section coordinates
+XtuRoot = XtuRt*c1;
+XtlRoot = XtlRt*c1;
+%Root airfoil area
+dx1 = XtuRoot(2,1)-XtuRoot(1,1);
+A1 = 0;
+for i = 9:31
+    tn = XtuRoot(i,2) - XtlRoot(i,2);
+    tn1 = XtuRoot(i+1,2) - XtlRoot(i+1,2);
+    Ai = dx1*(tn+tn1)/2;
+    A1 = A1 + Ai;
+end
+
+%Mid airfoil area
+XtuMid = XtuRt*c2;
+XtlMid = XtlRt*c2;
+dx2 = XtuMid(2,1)-XtuMid(1,1);
+A2 = 0;
+for i = 9:31
+    tn = XtuMid(i,2) - XtlMid(i,2);
+    tn1 = XtuMid(i+1,2) - XtlMid(i+1,2);
+    Ai = dx2*(tn+tn1)/2;
+    A2 = A2 + Ai;
+end
+
+
+%Outboard unit airfoil coordinates
+[Xtutip,Xtltip] = D_airfoil2(TipUp,TipLow,xairfoil);
+
+%Calculate c85 (85% semi-span) based on planform
+c85 = (c3-c2)/(y3-y2)*(0.9*y3 - y2) + c2;
+
+%85% span airfoil coordinates
+Xtutip = c85*Xtutip;
+Xtltip = c85*Xtltip;
+
+dx3 = Xtutip(2,1) - Xtutip(1,1);
+A3 = 0;
+
+%Loop to calculate cross-sectional area
+for i = 9:31
+    tn = Xtutip(i,2) - Xtltip(i,2);
+    tn1 = Xtutip(i+1,2) - Xtltip(i+1,2);
+    Ai = dx3*(tn+tn1)/2;
+    A3 = A3 + Ai;
+end
+
+%Tank Volume is average cross-sectional area times span
+
+%Inboard wing volume
+%Average area:
+Ain = (A1+A2)/2;
+
+%Volume
+Vin = Ain*y2;
+
+%Outboard wing volume
+%Average area:
+Aout = (A2+A3)/2;
+
+%Volume
+Vout = Aout*(0.9*y3 - y2);
+
+%Total volume of wing fuel tank:
+Vtot = 0.93*2*(Vin+Vout);
+
+%Calculate constraint
+Vreq = Wfuelhat/rhofuel;
+con2 = Vreq - Vaux - Vtot;
+
+c = [con1;con2];
